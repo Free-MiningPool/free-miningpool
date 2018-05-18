ed72a73f5e2ba5a0af39dc5f1cfbebbd3739c519
Léo Crotet <leo.crotet@nfrance.com>
2018-05-18 11:46:00 +0200

    Template PGC CLO AKA

---
+++ index.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" type="image/png" href="images/Freepool.png" />
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <title>Free Mining Pool</title>
+  </head>
+  <body>
+
+    <nav class="navbar navbar-expand-lg navbar-light mb-5">
+      <div class="container">
+        <div class="col-3 p-0 align-middle">
+          <a class="navbar-brand align-middle" href="#"><img class="logo mx-auto d-block float-left" src="images/Freepool.png" alt="logo"></a><a class="navbar-brand" href="#">Free Mining Pool</a>
+        </div>
+        <div class="col-9 justify-content">
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse float-right" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Our Pools
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item nav-b" href="#">AKA</a>
+                  <a class="dropdown-item nav-b" href="#">CLO</a>
+                  <a class="dropdown-item nav-b" href="#">PGC</a>
+                </div>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link nav-contact" href="#">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+
+    <div class="container">
+
+      <div class="pools-title container mb-4 p-1">
+        <h2 class="pools-h2 text-left mb-0">Our Pools</h2>
+      </div>
+
+      <div class="row">
+
+        <div class="col sm-6 p-3">
+          <div class="card">
+            <img class="card-img-top" src="images/aka_banner.jpg" alt="Card image cap">
+            <div class="card-body text-center">
+              <h5 class="card-title">Akroma Pool</h5>
+                <a href="#" class="btn btn-pool text-center">Let's mine !</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col sm-6 p-3">
+          <div class="card">
+            <img class="card-img-top" src="images/clo_banner2.png" alt="Card image cap">
+            <div class="card-body text-center">
+              <h5 class="card-title">Callisto Pool</h5>
+                <a href="#" class="btn btn-pool text-center">Let's mine !</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col sm-6 p-3">
+          <div class="card">
+            <img class="card-img-top" src="images/pgc_banner.png" alt="Card image cap">
+            <div class="card-body text-center">
+              <h5 class="card-title">PegasCoin Pool</h5>
+                <a href="#" class="btn btn-pool text-center">Let's mine !</a>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+
+    <div class="container mt-4 mb-5">
+
+      <div class="pools-title container mb-4 p-1">
+        <h2 class="pools-h2 text-left mb-0">Our Team</h2>
+      </div>
+
+      <div class="row mt-2">
+
+        <p class="pr-5 pl-5 text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p class="pr-5 pl-5 text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+      </div>
+
+    </div>
+
+
+
+    <div class="container-fluid footer">
+      <div class="container pt-4 pb-4">
+        <div class="row">
+            <div class="col">
+              <h4 class="foot-title">Our Pools</h4>
+              <ul class="nav flex-column">
+                <li class="nav-item">
+                  <a class="nav-link nav-foot" href="#">Akroma</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link nav-foot" href="#">Callisto</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link nav-foot" href="#">PegasCoins</a>
+                </li>
+              </ul>
+            </div>
+            <div class="col align-middle">
+              <h4 class="foot-title">Contact us !</h4>
+              <a class="nav-foot" href="#">support@free-miningpool.com</a><br><br>
+              <img class="logo mx-auto d-block float-left" src="images/logo_negatif.png" alt="logo">
+            </div>
+        </div>
+      </div>
+    </div>
+
+
+  </body>
+</html>
